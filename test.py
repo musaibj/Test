3157cc1c727324bf029e1aceb7bbcee3e6eb39f6
musaibj <musaib.j97@gmail.com>
2024-01-05 23:02:41 +0530

    Second commit

--- test.py
+++ test.py
@@ -5,16 +5,18 @@ def generate_username(first, second):
   usernames = []
   usernames.append(first + second)
   usernames.append(second + first)
-  usernames.append(first + "." + second)
-  usernames.append(first + "_" + second)
-  usernames.append(second + "." + first)
-  usernames.append(second + "_" + first)
-  usernames.append(first[0] + "." + second)
-  usernames.append(first[0] + "_" + second)
   usernames.append(first + second[0])
-  usernames.append(first + "_" + second[0])
+
+  usernames.append(first + "." + second)
+  usernames.append(second + "." + first)
+  usernames.append(first[0] + "." + second)
   usernames.append(first + "." + second[0])
   usernames.append(second[0] + "." + first)
+  
+  usernames.append(second + "_" + first)
+  usernames.append(first + "_" + second[0])
+  usernames.append(first[0] + "_" + second)
+  usernames.append(first + "_" + second)
   usernames.append(second[0] + "_" + first)
   return usernames
 
