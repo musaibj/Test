35de0a7c2d1363aa2f2d2e19f11ecdf52088b681
musaibj <musaib.j97@gmail.com>
2024-01-05 23:46:22 +0530

    sixth commit

--- api.py
+++ api.py
@@ -3,51 +3,45 @@ import json
 from test import Generator
 
 class MyHandler(BaseHTTPRequestHandler):
-    def response(self, responseCode, message):
-        self.send_response(responseCode)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')  # Allow any origin for testing
-        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')  # Specify allowed methods
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')  # Specify allowed headers
-        self.end_headers()
-        self.wfile.write(json.dumps(message, default=str).encode('utf-8'))
+  def response(self, responseCode, message):
+    self.send_response(responseCode)
+    self.send_header('Content-type', 'application/json')
+    self.send_header('Access-Control-Allow-Origin', '*')  # Allow any origin for testing
+    self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')  # Specify allowed methods
+    self.send_header('Access-Control-Allow-Headers', 'Content-Type')  # Specify allowed headers
+    self.end_headers()
+    self.wfile.write(json.dumps(message, default=str).encode('utf-8'))
 
-    def headerInfo(self):
-        contentLength = int(self.headers['Content-Length'])
-        getData = self.rfile.read(contentLength)
-        data = json.loads(getData.decode('utf-8'))
-        return data
+  def headerInfo(self):
+    contentLength = int(self.headers['Content-Length'])
+    getData = self.rfile.read(contentLength)
+    data = json.loads(getData.decode('utf-8'))
+    return data
 
-    def do_OPTIONS(self):
-        self.send_response(200, "OK")
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-        self.end_headers()
+  def do_OPTIONS(self):
+    self.send_response(200, "OK")
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+    self.end_headers()
 
-    def do_POST(self):
-        if self.path == '/get_input':
-            data = self.headerInfo()
-            first_name = data.get('FirstName')
-            second_name = data.get('SecondName')
-            usernames = obj.generate_username(first_name, second_name)
-            message = {'status': 'success', 'usernames': usernames}
-            self.response(200, message)
+  def do_POST(self):
+    if self.path == '/get_input':
+      data = self.headerInfo()
+      first_name = data.get('FirstName')
+      second_name = data.get('SecondName')
+      usernames = obj.generate_username(first_name, second_name)
+      message = {'status': 'success', 'usernames': usernames}
+      self.response(200, message)
 
-    def do_GET(self):
-        if self.path == '/generate_usernames':
-            message = {'status': 'success', 'message': 'Use POST request with /get_input to generate usernames'}
-            self.response(200, message)
+  def do_GET(self):
+    if self.path == '/generate_usernames':
+      message = {'status': 'success', 'message': 'Use POST request with /get_input to generate usernames'}
+      self.response(200, message)
 
-# Instantiate Generator class
 obj = Generator()
 
-# Instantiate MyHandler class
 handler = MyHandler
-
-# Create HTTP server
 server = HTTPServer(("localhost", 3036), handler)
 print("Server started on port 3036")
-
-# Start the server
 server.serve_forever()
